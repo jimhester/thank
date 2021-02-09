51fdd31acc735b1cdfcec8f6d1e96d23752fd944
Jim Hester <james.f.hester@gmail.com>
2021-02-09 14:51:16 -0500

    Actually use commas

--- data/thanks.R
+++ data/thanks.R
@@ -1,1 +1,1 @@
-delayedAssign("thanks", utils::read.table(system.file("extdata/thanks.csv", package = "thank"), header = TRUE, as.is = FALSE))
+delayedAssign("thanks", utils::read.table(system.file("extdata/thanks.csv", package = "thank"), sep = ",", header = TRUE, as.is = FALSE))
